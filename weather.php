a91f62a32a3d69fccc5d65f59195eb525c60ae5f
Jonathon Klem <jonathonklem@gmail.com>
2015-06-01 14:23:13 -0500

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,6 +1,5 @@
 <?php
 	// code from http://blog.worldweatheronline.com/2015/02/05/getting-started-world-weather-online-api-php/
-	
 	$queryUrl = "api.worldweatheronline.com/free/v2/weather.ashx?q=Evansville&format=json&num_of_days=1&key=ba7c662279e32364d70915bf44392";
 
 	/**
@@ -29,4 +28,5 @@
 	
 	$response = json_decode($result, true);
 	
-	echo "|Temp: " . $response['data']['current_condition'][0]['temp_F'] . "F|Humidity: " . $response['data']['current_condition'][0]['humidity']  . "|Description: " . $response['data']['current_condition'][0]['weatherDesc'][0]['value'];
+	// only print the necessary information	
+	echo "|Temp: " . $response['data']['current_condition'][0]['temp_F'] . "F|Humidity: " . $response['data']['current_condition'][0]['humidity']  . "|Descr: " . $response['data']['current_condition'][0]['weatherDesc'][0]['value'];
